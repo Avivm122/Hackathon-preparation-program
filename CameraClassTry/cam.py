d3d9fda1335f36c0dc4de9f1ae0573191420e2ae
Avivm122 <37801256+Avivm122@users.noreply.github.com>
2018-04-25 18:44:12 +0300

    Update cam.py

--- CameraClassTry/cam.py
+++ CameraClassTry/cam.py
@@ -7,6 +7,7 @@ import numpy as np
 
 class cam():
     def __init__(self):
+        self.cap = cv2.VideoCapture(0)
         self.lower_color = None
         self.upper_color = None
         self.frame = None
@@ -22,111 +23,58 @@ class cam():
         self.hsv = None
         self.mask = None
         self.res = None
+
             
-
-    def LiveCapture(self):
-        while True:
-            ret, self.frame = self.cap.read()
-
     def Capture(self):
         ret, self.frame = self.cap.read()
 
-    '''def ColorToHSV(self,b):
-        color = np.unit8(b)
-        hsv_color = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
-        return hsv_color'''
-
     def HSVImage(self):
         self.Capture()
-        self.hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    def getFrame(self):
-        return self.frame
-
-    def HSVImage(self,frame):
-        self.hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
 
     def MaskImage(self):
         self.HSVImage()
-        self.mask = cv2.inRange(hsv, self.lower_color, self.upper_color)
+        self.mask = cv2.inRange(self.hsv, self.lower_color, self.upper_color)
     
-    def MaskImage(self,hsv):
-        self.mask = cv2.inRange(hsv, self.lower_color, self.upper_color)
-
     def ResImage(self):
         self.MaskImage()
-        self.res = cv2.bitwise_and(frame,frame, mask=mask)
-
-    def ResImage(self,frame):
-        self.HSVImage(frame)
-        self.MaskImage(hsv)
-        self.res = cv2.bitwise_and(frame,frame, mask=mask)
+        self.res = cv2.bitwise_and(self.frame,self.frame,mask=self.mask)
     
     def ShowFrame(self):
         self.Capture()
         cv2.imshow('frame',self.frame)
 
-    '''def ShowFrame(self,frame):
-        cv2.imshow('frame',frame)'''
-
     def ShowHSV(self):
-        frame = Capture(self)
-        hsv = HSVImage(self,frame)
-        cv2.imshow('frame',frame)
-
-    def ShowHSV(self,frame):
-        hsv = HSVImage(self,frame)
-        cv2.imshow('HSV',hsv)
-
-    def ShowHSV(self,hsv):
-        cv2.imshow('HSV',hsv)
+        self.Capture()
+        self.HSVImage()
+        cv2.imshow('frame',self.hsv)
 
     def ShowMask(self):
-        frame = Capture(self)
-        hsv = HSVImage(self,frame)
-        mask = MaskImage(self,hsv)
-        cv2.imshow('Mask',mask)
-
-    def ShowMask(self,frame):
-        hsv = HSVImage(self,frame)
-        mask = MaskImage(self,hsv)
-        cv2.imshow('Mask',mask)
-
-    def ShowMask(self,hsv):
-        mask = MaskImage(self,hsv)
-        cv2.imshow('Mask',mask)
-
-    def ShowMask(self,mask):
-        cv2.imshow('Mask',mask)
+        self.Capture()
+        self.HSVImage()
+        self.MaskImage()
+        cv2.imshow('Mask',self.mask)
 
     def ShowRes(self):
-        frame = Capture(self)
-        hsv = HSVImage(self,frame)
-        mask = MaskImage(self,hsv)
-        res = ResImage(self,frame)
-        cv2.imshow('Res',res)
+        self.Capture()
+        self.HSVImage()
+        self.MaskImage()
+        self.ResImage()
+        cv2.imshow('Res',self.res)
 
     def ShowAll(self):
-        ShowFrame(self)
-        ShowHSV(self)
-        ShowMask(self)
-        ShowRes(self)
-
-    def ShowAll(self,frame):
-        ShowFrame(self,frame)
-        ShowHSV(self,frame)
-        ShowMask(self.frame)
-        ShowRes(self,frame)
+        self.ShowFrame()
+        self.ShowHSV()
+        self.ShowMask()
+        self.ShowRes()
 
     def Show_HSV_Mask_Res(self):
-        ShowHSV(self)
-        ShowMask(self)
-        ShowRes(self)
+        self.ShowHSV()
+        self.ShowMask()
+        self.ShowRes()
 
-    def Show_HSV_Mask_Res(self,frame):
-        ShowHSV(self,frame)
-        ShowMask(self,frame)
-        ShowRes(self,frame)
+    def release(self):
+        self.cap.release()
 
 
 
@@ -134,4 +82,9 @@ if __name__ == '__main__':
     lower_blue = np.array([110,50,50])
     upper_blue = np.array([130,255,255])
     cam = cam(lower_blue,upper_blue)
-    cam.ShowFrame()
+    while True:
+        cam.ShowAll()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cam.cap.release()
+    cv2.destroyAllWindows()
